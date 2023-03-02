d6802df7530c5339834684f344a96dcb2ecbe803
Furkan Yildirim <furkanyildirim3455.3455@gmail.com>
2023-03-02 17:21:33 +0100

    home page added

--- src/main/java/be/thomasmore/travelwebapp/TravelWebAppApplication.java
+++ src/main/java/be/thomasmore/travelwebapp/TravelWebAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelWebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelWebAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelWebAppApplication.class, args);
+    }
 
 }
