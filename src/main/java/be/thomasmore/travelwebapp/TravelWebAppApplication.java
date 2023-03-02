8fe208926621cd22b88c2fd292c35f33f58de3b6
Furkan Yildirim <furkanyildirim3455.3455@gmail.com>
2023-03-02 16:54:13 +0100

    Initial commit

---
+++ src/main/java/be/thomasmore/travelwebapp/TravelWebAppApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.travelwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/thomasmore/travelwebapp/TravelWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.travelwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
