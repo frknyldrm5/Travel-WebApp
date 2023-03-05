a7c955d612b90acda32ab493b4212c7a8fc00fe9
Furkan Yildirim <furkanyildirim3455.3455@gmail.com>
2023-03-05 21:46:45 +0100

    bundel 2 study

--- src/main/java/be/thomasmore/travelwebapp/controllers/VenueController.java
+++ src/main/java/be/thomasmore/travelwebapp/controllers/VenueController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -14,8 +15,11 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
-    @GetMapping("/venuedetails")
-    public String venueDetails(Model model){
+    @GetMapping({"/venuedetails","/venuedetails/{id}"})
+    public String venueDetails(Model model, @PathVariable(required = false) Integer id){
+        if (id==null){
+            return "venuedetails";
+        }
 //        Venue venue=new Venue("Roma","https://shoppenin.mechelen.be/blog-boejskammeree");
 //        model.addAttribute("venue",venue);
 //        venue.setCapacity(2000);
@@ -25,11 +29,21 @@ public class VenueController {
 //        venue.setFreeParkingAvailable(true);
 //        venue.setDistanceFromPublicTransportInKm(2.4);
 
-        Optional<Venue> optionalVenue=venueRepository.findById(1);
+        Optional<Venue> optionalVenue=venueRepository.findById(id);
         if (optionalVenue.isPresent()){
             model.addAttribute("venue",optionalVenue.get());
         }
 
         return "venuedetails";
     }
+
+
+
+
+
+
+
+
+
+
 }
